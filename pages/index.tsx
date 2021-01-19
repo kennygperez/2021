9dc6464a8cea09a6806c232be6c8ffbaf9bf54b4
Kenny <kennygperez@gmail.com>
2021-01-18 20:42:00 -0500

    Added seo meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,32 @@
-// import Head from 'next/head';
+import Head from 'next/head';
 import App from '../components/App';
 import Splash from '../components/Splash';
 
-const index = () => (
-	<>
-		<Splash />
-		<App />
-	</>
-);
+const index = () => {
+	const seo = {
+		title: 'Kenny G Perez',
+		url: 'https://www.kennygperez.com/',
+		description: 'Kenny G Perez software engineer based in Orlando',
+	};
+
+	return (
+		<>
+			<Head>
+				<title>{seo.title}</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta property='og:type' content='website' key='type' />
+				<meta property='og:title' content={seo.title} key='title' />
+				<meta property='og:url' content={seo.url} key='url' />
+				<meta
+					property='og:description'
+					content={seo.description}
+					key='description'
+				/>
+			</Head>
+			<Splash />
+			<App />
+		</>
+	);
+};
 
 export default index;
